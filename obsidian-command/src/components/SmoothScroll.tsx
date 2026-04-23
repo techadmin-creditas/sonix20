3fb79cdf36f8f296bd92eaccc8a2908d75aef5d2
CodePilot <codepilot@example.com>
2026-04-23 13:46:48 +0530

    smooth scroll

--- obsidian-command/src/components/SmoothScroll.tsx
+++ obsidian-command/src/components/SmoothScroll.tsx
@@ -6,7 +6,7 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger';
 export function SmoothScroll() {
     useEffect(() => {
         const lenis = new Lenis({
-            duration: 0.2,
+            duration: 0.5,
             easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
             orientation: 'vertical',
             gestureOrientation: 'vertical',
